00000004c45f3d664e6872a99588a0950d5b7f33
Soorria Saruva <soorria.ss@gmail.com>
2025-02-06 23:21:49 +1100

    fix: dumb mode

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,15 @@
 import { handle } from '@hono/node-server/vercel'
 import { createApp } from '../src/app'
 import { IncomingMessage, ServerResponse } from 'node:http'
+import { runPythonCode } from '../src/lib/python-execution/run-code'
 
-export default (req: IncomingMessage, res: ServerResponse) => {
+export default async (req: IncomingMessage, res: ServerResponse) => {
   console.log('Node request', req.url)
-  return handle(createApp().mount('/api', createApp().fetch))(req, res)
+  // return handle(createApp().mount('/api', createApp().fetch))(req, res)
+
+  res.statusCode = 200
+  res.write(JSON.stringify(await runPythonCode({ code: '1 + 1' })))
+  res.end()
+
+  return
 }
